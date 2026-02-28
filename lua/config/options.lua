4684e84c7ed4cdac941a50e95223e5386b86a79c
ceehel <lauper.cyril@gmail.com>
2026-02-28 08:40:33 +0100

    adapting from LazyVim

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 10
